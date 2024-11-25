5e374cb56502a0c1ea3c5305c8df5b4c08cf41e2
Hajar <hajarmoubhale@gmail.com>
2024-11-25 08:45:01 +0100

    mas cambios

--- src/org/ies/vehicles/Main.java
+++ src/org/ies/vehicles/Main.java
@@ -1,26 +1,27 @@
 package org.ies.vehicles;
 
-import org.ies.vehicles.model.VehicleType;
+import org.ies.vehicles.components.GarageReader;
+import org.ies.vehicles.components.VehicleReader;
+import org.ies.vehicles.model.Garage;
+import org.ies.vehicles.model.Vehicle;
 
-import java.util.Objects;
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Vehicle vehicle = new vehicle (
-            VehicleType.Car,
+        // Creacion de componentes
+        Scanner scanner = new Scanner(System.in);
+        VehicleReader vehicleReader = new VehicleReader(scanner);
+        GarageReader garageReader = new GarageReader(scanner, vehicleReader);
 
+        // Ejecucion del programa
+        Garage garage = garageReader.read();
 
-                );
-
-
-        }
-
+        // Muestra el resultado
+        System.out.println(garage);
 
     }
 }
-
-
 
 
 
